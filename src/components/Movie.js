54ec1182f5e792cfae2efb775f5f940093e94b3a
unknown <seivey@hotmail.com>
2021-07-25 21:33:27 -0600

    mvp done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useParams, useHistory } from 'react-router-dom';
+import MovieList from './MovieList';
+import { connect } from 'react-redux'
 
-const Movie = (props) => {
+export const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const movies = [];
+    const movies = props.movies;
     const movie = movies.find(movie=>movie.id===Number(id));
     
     return(<div className="modal-page col">
@@ -47,4 +49,10 @@ const Movie = (props) => {
     </div>);
 }
 
-export default Movie;
+
+const mapStateToProps = state => {
+    return{ 
+    movies: state.movies,
+    }
+}
+export default connect(mapStateToProps)(Movie)
